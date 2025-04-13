e108f37b430b7e4d5d9776c47ee8b425050b8753
ërnestegisubizo83@gmail.com <rnestegisubizo83@gmail.com>
2025-04-13 22:55:13 +0400

    v

---
+++ x.py
@@ -0,0 +1,9 @@
+class CLASS_NAME:
+    def __init__(self, name, age, national):
+        self.name=name
+        self.age=age
+        print("i am a teacher"+name)
+        cust = CLASS_NAME()
+        cust.customer("kagabo", 20, "rwanda")
+        cust.customer()
+
